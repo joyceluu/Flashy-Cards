d0ba136396f6812a45d6bfe067926e3336b84cdb
Joyce Luu <atsukara@gmail.com>
2018-10-13 15:08:07 -0700

    first commit

---
+++ app/src/main/java/com/example/joy/flashycards/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.joy.flashycards;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+        ImageView ico = (ImageView) findViewById(R.id.toggle_choices_visibility);
+        ico.setOnClickListener(new View.OnClickListener() {
+            boolean isShowingAnswers = false;
+            @Override
+            public void onClick(View v) {
+                if (isShowingAnswers) {
+                    findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
+                    isShowingAnswers = false;
+                }
+                else {
+                    findViewById(R.id.answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer3).setVisibility(View.VISIBLE);
+                    isShowingAnswers = true;
+                }
+
+            }
+        });
+
+        findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer1).setBackgroundColor(getColor(R.color.wrong));
+            }
+        });
+
+        findViewById(R.id.answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer2).setBackgroundColor(getColor(R.color.correct));
+            }
+        });
+
+        findViewById(R.id.answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer3).setBackgroundColor(getColor(R.color.wrong));
+            }
+        });
+    }
+}
